4d546f98e08362a09e6a5490f59a6238aa2a94b0
Durant Schoon <durant.schoon@gmail.com>
2022-10-05 14:51:45 -0400

    rename variable to file_msg

--- unix_work_or_home.sh
+++ unix_work_or_home.sh
@@ -2,13 +2,13 @@
 
 # my ./aliases will check for the existence of a ~/.HOME or ~/.WORK file to determine which aliases to load
 
-msg="See ~/.aliases for the use of this file"
+file_msg="See ~/.aliases for the use of this file"
 
 echo -n "Are you setting up dot files for HOME or WORK [h|w]? "
 
 read -r answer
 
-[[ "$answer" = h* ]] && touch ~/.HOME && echo $msg >> ~/.HOME && echo You are now set up for HOME && exit 0
-[[ "$answer" = w* ]] && touch ~/.WORK && echo $msg >> ~/.WORK && echo You are now set up for WORK && exit 0
+[[ "$answer" = h* ]] && touch ~/.HOME && echo $file_msg >> ~/.HOME && echo You are now set up for HOME && exit 0
+[[ "$answer" = w* ]] && touch ~/.WORK && echo $file_msg >> ~/.WORK && echo You are now set up for WORK && exit 0
 
 echo Did not understand your answer && exit 1
