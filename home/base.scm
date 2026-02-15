853889f3e779a3bdd5be23b7e6643135b1bd7a4c
Durant Schoon <durant.schoon@gmail.com>
2026-02-14 22:43:22 -0500

    Fix Zsh configuration for Guix Home
    - Refactor home/base.scm to use home-zsh-configuration fields
    - Condition source commands in .zshrc.starship
    - Fix syntax error in .aliases
    - Add poetry command check in .shared.zshrc

--- home/base.scm
+++ home/base.scm
@@ -2,6 +2,7 @@
              (gnu home services)
              (gnu home services shells)
              (gnu home services shepherd)
+             (gnu packages)
              (guix gexp))
 
 (home-environment
@@ -11,46 +12,46 @@
   (services
    (list
     ;; Emacs daemon for fast emacsclient
-    (service home-shepherd-service-type
-      (home-shepherd-configuration
-       (services (list
-                  (shepherd-service
-                   (provision '(emacs))
-                   (documentation "Emacs user daemon.")
-                   (start #~(make-forkexec-constructor
-                             (list #$(file-append (specification->package "emacs") "/bin/emacs")
-                                   "--fg-daemon")))
-                   (stop  #~(make-kill-destructor))
-                   (auto-start? #t))))))
+    ;; (service home-shepherd-service-type
+    ;;   (home-shepherd-configuration
+    ;;    (services (list
+    ;;               (shepherd-service
+    ;;                (provision '(emacs))
+    ;;                (documentation "Emacs user daemon.")
+    ;;                (start #~(make-forkexec-constructor
+    ;;                          (list #$(file-append (specification->package "emacs") "/bin/emacs")
+    ;;                                "--fg-daemon")))
+    ;;                (stop  #~(make-kill-destructor))
+    ;;                (auto-start? #t))))))
 
     ;; Zsh + Starship + editor aliases
     (service home-zsh-service-type
       (home-zsh-configuration
        (zshrc
-        '(
-          "export EDITOR='emacsclient -c -a \"\"'"
-          "export VISUAL=\"$EDITOR\""
-          "eval \"$(starship init zsh)\""
-          "alias e='emacsclient -c -a \"\"'"
-          "alias ec='emacsclient -t -a \"\"'"
-          "alias ll=\"ls -lah\""
-          "source ~/.shared.zshenv"
-          "source ~/.shared.zshrc"
-        ))))
-
-    ;; Link your existing dotfiles if they exist in repo root
-    (simple-service 'durant-dotfiles home-files-service-type
-      (append
-        (filter identity
-         (list
-           (and (file-exists? ".zshrc.starship") `("zshrc" ,(local-file ".zshrc.starship")))
-           (and (file-exists? ".shared.zshenv") `("shared.zshenv" ,(local-file ".shared.zshenv")))
-           (and (file-exists? ".shared.zshrc")  `("shared.zshrc"  ,(local-file ".shared.zshrc")))
-           (and (file-exists? ".aliases")       `("aliases"       ,(local-file ".aliases")))
-           (and (file-exists? ".zprofile")      `("zprofile"      ,(local-file ".zprofile")))
-           (and (file-exists? ".linux.zshenv")  `("linux.zshenv"  ,(local-file ".linux.zshenv")))
-           (and (file-exists? ".mac.zshenv")    `("mac.zshenv"    ,(local-file ".mac.zshenv")))
-           (and (file-exists? ".work.zshrc")    `("work.zshrc"    ,(local-file ".work.zshrc")))
-         )))
-      )
+        (list
+         (plain-file "zsh-extra-config"
+          (string-append
+           "export EDITOR='emacsclient -c -a \"\"'\n"
+           "export VISUAL=\"$EDITOR\"\n"
+           "eval \"$(starship init zsh)\"\n"
+           "alias e='emacsclient -c -a \"\"'\n"
+           "alias ec='emacsclient -t -a \"\"'\n"
+           "alias ll=\"ls -lah\"\n"
+          ))
+         (local-file "../.zshrc.starship" "zshrc.starship")
+         (local-file "../.shared.zshrc" "shared.zshrc")
+         (local-file "../.aliases" "aliases")
+         ;; (local-file "../.work.zshrc" "work.zshrc") ;; Uncomment if needed
+        ))
+       (zshenv
+        (list
+         (local-file "../.shared.zshenv" "shared.zshenv")
+         (local-file "../.linux.zshenv" "linux.zshenv")
+         ;; (local-file "../.mac.zshenv" "mac.zshenv") ;; Uncomment if needed
+        ))
+       (zprofile
+        (list
+         (local-file "../.zprofile" "zprofile")
+        ))
+      ))
    )))
