d377bc38bcb16c26225ac3d74174db8168e6a389
Durant Schoon <durant.schoon@gmail.com>
2026-02-14 23:28:36 -0500

    fix(home): robustness - check for .git dir not just .emacs.d

--- home/base.scm
+++ home/base.scm
@@ -8,21 +8,38 @@
 (home-environment
   (packages
    (specifications->packages
-    '("git" "zsh" "starship" "ripgrep" "fd" "fzf" "eza" "emacs")))
+    '("git" "zsh" "starship" "ripgrep" "fd" "fzf" "eza" "emacs" "font-adobe-source-code-pro" "nss-certs")))
   (services
    (list
     ;; Emacs daemon for fast emacsclient
-    ;; (service home-shepherd-service-type
-    ;;   (home-shepherd-configuration
-    ;;    (services (list
-    ;;               (shepherd-service
-    ;;                (provision '(emacs))
-    ;;                (documentation "Emacs user daemon.")
-    ;;                (start #~(make-forkexec-constructor
-    ;;                          (list #$(file-append (specification->package "emacs") "/bin/emacs")
-    ;;                                "--fg-daemon")))
-    ;;                (stop  #~(make-kill-destructor))
-    ;;                (auto-start? #t))))))
+    (service home-shepherd-service-type
+      (home-shepherd-configuration
+       (services (list
+                  (shepherd-service
+                   (provision '(emacs))
+                   (documentation "Emacs user daemon.")
+                   (start #~(make-forkexec-constructor
+                             (list #$(file-append (specification->package "emacs") "/bin/emacs")
+                                   "--fg-daemon")))
+                   (stop  #~(make-kill-destructor))
+                   (auto-start? #t))))))
+
+    ;; Ensure Spacemacs and config are present
+    (simple-service 'spacemacs-activation
+                    home-activation-service-type
+                    #~(begin
+                        (let* ((emacs-d (string-append (getenv "HOME") "/.emacs.d"))
+                               (spacemacs-d (string-append (getenv "HOME") "/.spacemacs.d"))
+                               (git (string-append #$(specification->package "git") "/bin/git"))
+                               (certs (string-append #$(specification->package "nss-certs") "/etc/ssl/certs/ca-certificates.crt")))
+                          ;; Clone Spacemacs (upstream)
+                          (unless (file-exists? (string-append emacs-d "/.git"))
+                            (format #t "Cloning Spacemacs to ~a...~%" emacs-d)
+                            (system* git "-c" (string-append "http.sslCAInfo=" certs) "clone" "-b" "develop" "https://github.com/syl20bnr/spacemacs" emacs-d))
+                          ;; Clone User Config
+                          (unless (file-exists? spacemacs-d)
+                            (format #t "Cloning local Spacemacs config to ~a...~%" spacemacs-d)
+                            (system* git "-c" (string-append "http.sslCAInfo=" certs) "clone" "https://github.com/durantschoon/.spacemacs.d" spacemacs-d)))))
 
     ;; Zsh + Starship + editor aliases
     (service home-zsh-service-type
@@ -33,6 +50,7 @@
           (string-append
            "export EDITOR='emacsclient -c -a \"\"'\n"
            "export VISUAL=\"$EDITOR\"\n"
+           "export SPACEMACSDIR=\"$HOME/.spacemacs.d\"\n"
            "eval \"$(starship init zsh)\"\n"
            "alias e='emacsclient -c -a \"\"'\n"
            "alias ec='emacsclient -t -a \"\"'\n"
