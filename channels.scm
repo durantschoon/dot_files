377d967d5a5e3a10e94a3e030fc00540ee6018c2
Durant Schoon <durant.schoon@gmail.com>
2026-02-26 23:59:36 -0500

    fix(guix): resolve structural errors and pin stable channel
    - Fix parenthesis nesting in wayland.scm and base.scm
    - Pin guix channel to stable commit 17c21422 to avoid broken rebuilds
    - Update Makefile with --allow-downgrades for robust reconfigurations

--- channels.scm
+++ channels.scm
@@ -3,6 +3,7 @@
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
   (branch "master")
+  (commit "17c2142271bb4857cc0e517ac42af50b4a55aef6")
   (introduction
    (make-channel-introduction
     "9edb3f66fd807b096b48283debdcddccfea34bad"
